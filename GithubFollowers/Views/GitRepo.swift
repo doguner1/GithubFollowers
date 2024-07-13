ea0af26b9cdda0ef6036cea3080a83f413257328
doguner1 <d.gunr@yaani.com>
2024-07-13 19:49:21 +0300

    first commit

---
+++ GithubFollowers/Views/GitRepo.swift
@@ -0,0 +1,18 @@
+//
+//  GitRepo.swift
+//  GithubFollowers
+//
+//  Created by qwerty on 13.07.2024.
+//
+
+import SwiftUI
+
+struct GitRepo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitRepo()
+}
